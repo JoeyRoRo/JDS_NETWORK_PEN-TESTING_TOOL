b70df8d9fb726cc20e657e3c9b41dc1cdd5e6bc1
bboybreeze <joejoejoey13@gmail.com>
2015-07-08 16:33:14 -0400

    Create myapp.py

---
+++ myapp.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python2
+from __future__ import print_function
+import consolemenu
+
+
+app_title = '{}\n## JDS NETWORK PEN-TESTING TOOL ##\n{}'.format('#'*34,'#'*34
+)
+cm = consolemenu.ConsoleMenu('menu',title=app_title)
+cm.start() 
