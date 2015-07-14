5beb6e537becdd6767cda8aafe3e72a0684f0b15
bboybreeze <joejoejoey13@gmail.com>
2015-07-13 23:45:21 -0400

    Delete jdstool.py

--- jdstool.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python2
-from __future__ import print_function
-import consolemenu
-
-
-app_title = '{}\n## JDS PEN-TESTING TOOL ##\n{}'.format('#'*26, '#'*26)
-cm = consolemenu.ConsoleMenu('menu',title=app_title)
-cm.start() 
